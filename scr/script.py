2e41ed593829bac320bfd25d1523e218415fd94c
Marcio Candido <90878983+Marcio-Candido@users.noreply.github.com>
2025-02-10 13:56:24 -0300

    Update script.py

--- scr/script.py
+++ scr/script.py
@@ -2,6 +2,10 @@ arq = 'imagens/data.txt'
 # lendo o arquivo
 with open(arq,'r') as f:
   numero = f.read()
+  f.close()
 numero = int(numero)
-with open(arq,'w') as f2:
-  print(numero+1,f2)
+numero = numero + 1 
+print(numero)
+with open(arq,'w') as f:
+  print(numero,f)
+  f.close()
